85c3434ee54bd20813d21c441e02ba51c53180dc
Youri Novikov <73738882+0youri@users.noreply.github.com>
2023-03-06 15:19:29 +0100

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    server: {
-        port: 3300
-    }
+    modules: ['@nuxtjs/tailwindcss']
 })
